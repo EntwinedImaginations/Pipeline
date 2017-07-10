5bbeca8072f6a78a6dc6eca253c063568d37ec2f
zayed-mbd <zayed_mbd@yahoo.com>
2017-07-10 22:16:03 +1000

    Project set up

---
+++ PipelineXamarine/PipelineXamarinePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PipelineXamarine
+{
+	public partial class PipelineXamarinePage : TabbedPage
+	{
+		public PipelineXamarinePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
